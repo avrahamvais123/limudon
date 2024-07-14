065995a87dc6964dcd6d7d63bf670e460465873e
avrahamvais123 <avrahamvais123@gmail.com>
2024-07-14 17:37:06 +0300

    fix matcher in middleware

--- src/middleware.js
+++ src/middleware.js
@@ -9,25 +9,14 @@ export async function middleware(request) {
   console.log("Refresh Token:", refreshToken);
   console.log("URL:", url.pathname);
 
-  // רשימת נתיבים שמוחרגים מהבדיקה
-  const excludedPaths = [
-    "/auth/login",
-    "/auth/signup",
-    "/api/auth/google",
-    "/",
-  ];
-
-  // בדיקה אם הנתיב נמצא תחת /api או /auth או הוא אחד מהנתיבים המוחרגים המדויקים
-  const isExcluded = excludedPaths.some(
-    (path) =>
-      url.pathname === path ||
-      url.pathname.startsWith("/api") ||
-      url.pathname.startsWith("/auth")
-  );
+  const isExcluded =
+    url.pathname === "/" ||
+    url.pathname.startsWith("/api") ||
+    url.pathname.startsWith("/auth");
 
   console.log("isExcluded:", isExcluded);
 
-  if (!refreshToken && !isExcluded) {
+  if (!token && !refreshToken && !isExcluded) {
     console.log("No token found, redirecting to login");
     return NextResponse.redirect(new URL("/auth/login", request.url));
   }
@@ -82,6 +71,25 @@ export async function middleware(request) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|logo.ico|logo-limudon.png|books-background.jpg|auth/login|api/auth/signup|api/auth/login|auth/signup|api/auth/google|api/auth/callback/google|$).*)",
+    "/((?!_next/static|_next/image|logo.ico|logo-limudon.png|books-background.jpg|$).*)",
   ],
 };
+
+// auth/login|api/auth/signup|api/auth/login|auth/signup|api/auth/google|api/auth/callback/google|
+
+/* // רשימת נתיבים שמוחרגים מהבדיקה
+  const excludedPaths = [
+    "/auth/login",
+    "/auth/signup",
+    "/api/auth/google",
+    "/api/auth/callback/google",
+    "/",
+  ];
+
+  // בדיקה אם הנתיב נמצא תחת /api או /auth או הוא אחד מהנתיבים המוחרגים המדויקים
+  const isExcluded = excludedPaths.some(
+    (path) =>
+      url.pathname === path ||
+      url.pathname.startsWith("/api") ||
+      url.pathname.startsWith("/auth")
+  ); */
